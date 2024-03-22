69e888e0410fef4a0614842b3de32dffec6a78e0
Rahma Ali <rahma.ma078@gmail.com>
2024-03-22 16:56:05 +0000

    update mock data

--- src/components/Grid/index.jsx
+++ src/components/Grid/index.jsx
@@ -11,25 +11,127 @@ export default function ConversationsGrid() {
             // const data = await response.json()
             const data = [
                 {
-                  image: "https://example.com/image1.jpg",
-                  id: "1",
-                  name: "Conversation 1",
-                  language: "English"
+                    image: "https://th.bing.com/th/id/OIP.Nad5BE0CntDSooa00dmyTAHaHa?rs=1&pid=ImgDetMain",
+                    id: "1",
+                    name: "Conversation 1",
+                    language: "English"
                 },
                 {
-                  image: "https://example.com/image2.jpg",
-                  id: "2",
-                  name: "Conversation 2",
-                  language: "Spanish"
+                    image: "https://th.bing.com/th/id/OIP.Nad5BE0CntDSooa00dmyTAHaHa?rs=1&pid=ImgDetMain",
+                    id: "2",
+                    name: "Conversation 2",
+                    language: "Spanish"
                 },
                 {
-                  image: "https://example.com/image3.jpg",
-                  id: "3",
-                  name: "Conversation 3",
-                  language: "French"
+                    image: "https://th.bing.com/th/id/OIP.Nad5BE0CntDSooa00dmyTAHaHa?rs=1&pid=ImgDetMain",
+                    id: "3",
+                    name: "Conversation 3",
+                    language: "French"
+                },
+                {
+                    image: "https://th.bing.com/th/id/OIP.Nad5BE0CntDSooa00dmyTAHaHa?rs=1&pid=ImgDetMain",
+                    id: "4",
+                    name: "Conversation 4",
+                    language: "German"
+                },
+                {
+                    image: "https://th.bing.com/th/id/OIP.Nad5BE0CntDSooa00dmyTAHaHa?rs=1&pid=ImgDetMain",
+                    id: "5",
+                    name: "Conversation 5",
+                    language: "Italian"
+                },
+                {
+                    image: "https://th.bing.com/th/id/OIP.Nad5BE0CntDSooa00dmyTAHaHa?rs=1&pid=ImgDetMain",
+                    id: "6",
+                    name: "Conversation 6",
+                    language: "Portuguese"
+                },
+                {
+                    image: "https://th.bing.com/th/id/OIP.Nad5BE0CntDSooa00dmyTAHaHa?rs=1&pid=ImgDetMain",
+                    id: "7",
+                    name: "Conversation 7",
+                    language: "Chinese"
+                },
+                {
+                    image: "https://th.bing.com/th/id/OIP.Nad5BE0CntDSooa00dmyTAHaHa?rs=1&pid=ImgDetMain",
+                    id: "8",
+                    name: "Conversation 8",
+                    language: "Japanese"
+                },
+                {
+                    image: "https://th.bing.com/th/id/OIP.Nad5BE0CntDSooa00dmyTAHaHa?rs=1&pid=ImgDetMain",
+                    id: "9",
+                    name: "Conversation 9",
+                    language: "Korean"
+                },
+                {
+                    image: "https://th.bing.com/th/id/OIP.Nad5BE0CntDSooa00dmyTAHaHa?rs=1&pid=ImgDetMain",
+                    id: "10",
+                    name: "Conversation 10",
+                    language: "Arabic"
+                },
+                {
+                    image: "https://th.bing.com/th/id/OIP.Nad5BE0CntDSooa00dmyTAHaHa?rs=1&pid=ImgDetMain",
+                    id: "11",
+                    name: "Conversation 11",
+                    language: "Russian"
+                },
+                {
+                    image: "https://th.bing.com/th/id/OIP.Nad5BE0CntDSooa00dmyTAHaHa?rs=1&pid=ImgDetMain",
+                    id: "12",
+                    name: "Conversation 12",
+                    language: "Hindi"
+                },
+                {
+                    image: "https://th.bing.com/th/id/OIP.Nad5BE0CntDSooa00dmyTAHaHa?rs=1&pid=ImgDetMain",
+                    id: "13",
+                    name: "Conversation 13",
+                    language: "Bengali"
+                },
+                {
+                    image: "https://th.bing.com/th/id/OIP.Nad5BE0CntDSooa00dmyTAHaHa?rs=1&pid=ImgDetMain",
+                    id: "14",
+                    name: "Conversation 14",
+                    language: "Punjabi"
+                },
+                {
+                    image: "https://th.bing.com/th/id/OIP.Nad5BE0CntDSooa00dmyTAHaHa?rs=1&pid=ImgDetMain",
+                    id: "15",
+                    name: "Conversation 15",
+                    language: "Turkish"
+                },
+                {
+                    image: "https://th.bing.com/th/id/OIP.Nad5BE0CntDSooa00dmyTAHaHa?rs=1&pid=ImgDetMain",
+                    id: "16",
+                    name: "Conversation 16",
+                    language: "Dutch"
+                },
+                {
+                    image: "https://th.bing.com/th/id/OIP.Nad5BE0CntDSooa00dmyTAHaHa?rs=1&pid=ImgDetMain",
+                    id: "17",
+                    name: "Conversation 17",
+                    language: "Swedish"
+                },
+                {
+                    image: "https://th.bing.com/th/id/OIP.Nad5BE0CntDSooa00dmyTAHaHa?rs=1&pid=ImgDetMain",
+                    id: "18",
+                    name: "Conversation 18",
+                    language: "Polish"
+                },
+                {
+                    image: "https://th.bing.com/th/id/OIP.Nad5BE0CntDSooa00dmyTAHaHa?rs=1&pid=ImgDetMain",
+                    id: "19",
+                    name: "Conversation 19",
+                    language: "Greek"
+                },
+                {
+                    image: "https://th.bing.com/th/id/OIP.Nad5BE0CntDSooa00dmyTAHaHa?rs=1&pid=ImgDetMain",
+                    id: "20",
+                    name: "Conversation 20",
+                    language: "Finnish"
                 }
-              ]
-              
+            ];
+                          
             setConversation(data)
         }
         fetchConversations()
