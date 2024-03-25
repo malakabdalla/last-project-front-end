247163a2493e1080e71075fb51a557e54e6a1480
hortfrancis <ajhf@hotmail.co.uk>
2024-03-25 17:46:48 +0000

    Improve styling for Language page & link to Conversation page

--- src/components/Languages/index.jsx
+++ src/components/Languages/index.jsx
@@ -1,14 +1,23 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./LanguageSelector.css";
 const LanguageSelector = () => {
   const [selectedLanguage, setSelectedLanguage] = useState("");
+  const navigate = useNavigate();
 
   const handleLanguageSelect = (language) => {
     setSelectedLanguage(language);
   };
 
+  const startPractising = () => {
+    console.log("Selected language: ", selectedLanguage);
+    // Mocked for demo
+    // Redirect to the conversation page
+    navigate('/conversation/1');
+  }
+
   return (
-    <div className="language-selector">
+    <div className="language-selector max-w-2xl mx-auto">
       <div className="language-options">
         <label
           htmlFor="Arabic"
@@ -118,6 +127,14 @@ const LanguageSelector = () => {
           />
           <span>Polish</span>
         </label>
+      </div>
+      <div className="pt-16 flex justify-center">
+        <button
+          className="bg-rose-500 text-white font-bold py-2 px-4 rounded"
+          onClick={startPractising}
+        >
+          Start practicing
+        </button>
       </div>
     </div>
   );
