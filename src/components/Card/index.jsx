1ba9e8e2f9b84743ae39bdd2844993920857f45f
Rahma Ali <rahma.ma078@gmail.com>
2024-03-25 08:47:20 +0000

    update card after using POST request

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -3,11 +3,13 @@ import './card.css'
 
 export default function ConversationCard({ conversation }) {
 
+    const conversationsImage = "https://th.bing.com/th/id/OIP.Nad5BE0CntDSooa00dmyTAHaHa?rs=1&pid=ImgDetMain"
+
     return (
         <div className="conversation-card">
             <h2>{conversation.language}</h2>
             <div> 
-                <img src={conversation.image} alt={conversation.id} />
+                <img src={conversationsImage} alt={conversation.conversation_id} />
             </div>
         </div>
     )
