44c33db09d2f165ed5a662e36ffd48e612b6e342
Rahma Ali <rahma.ma078@gmail.com>
2024-03-25 08:56:16 +0000

    update in line with component changes

--- src/pages/DashboardPage/index.jsx
+++ src/pages/DashboardPage/index.jsx
@@ -1,10 +1,19 @@
+import React, { useEffect, useHistory } from 'react';
+import { Link } from 'react-router-dom';
 import ConversationsGrid from "../../components/Grid";
+import RecentConversationsGrid from '../../components/Grid/recent';
+import NewChatBtn from '../../components/NewChatBtn';
+import "./dashboard.css"
 
 export default function DashboardPage() {
+
   return (
   <>
-    DashboardPage
+    <NewChatBtn />
+    <Link id="link">New Conversations →</Link>
     <ConversationsGrid />
+    <h1>Recently Viewed</h1>
+    <RecentConversationsGrid />
   </>
   );
 }
