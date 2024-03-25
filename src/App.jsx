8bcabacb8147967250add86ceb061bc73df5bd93
Rahma Ali <rahma.ma078@gmail.com>
2024-03-25 08:45:33 +0000

    add conversations provider and protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,31 @@ import { Route, Routes } from "react-router-dom";
 import "./index.css";
 import * as Pages from "./pages";
 import { ThemeProvider } from "./context/ThemeContext";
-import { ConversationProvider } from "./context/Conversations";
+import { ConversationsProvider } from "./context/Conversations";
+import { AuthProvider } from "./hooks/useAuth";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function App() {
   return (
-    <ThemeProvider>
-      <ConversationProvider>
-        <Routes>
-          <Route path="/" element={<PageWrapper />}>
-            <Route index element={<Pages.LandingPage />} />
-            <Route path="*" element={<Pages.NotFound />} />
-          </Route>
-          <Route path="/login" element={<Pages.LoginPage />} />
-          <Route path="/register" element={<Pages.RegisterPage />} />
-          <Route path="/auth" element={<PageWrapperAuth />}>
-          <Route path="language" element={<Pages.LanguagePage />} />
-          <Route path="dashboard" element={<Pages.DashboardPage />} />
-          <Route path="conversation" element={<Pages.ConversationPage />} />
-          </Route>
-        </Routes>
-      </ConversationProvider>
-    </ThemeProvider>
+    <AuthProvider>
+      <ThemeProvider>
+        <ConversationsProvider>
+          <Routes>
+            <Route path="/" element={<PageWrapper />}>
+              <Route index element={<Pages.LandingPage />} />
+              <Route path="*" element={<Pages.NotFound />} />
+            </Route>
+            <Route path="/login" element={<Pages.LoginPage />} />
+            <Route path="/register" element={<Pages.RegisterPage />} />
+            <Route path="/user" element={<ProtectedRoute><PageWrapperAuth /></ProtectedRoute>}>
+            <Route path="dashboard" element={<Pages.DashboardPage />} />
+            <Route path="language" element={<Pages.LanguagePage />} />
+            <Route path="allconversations/:id" element={<Pages.AllConversationsPage />} />
+            <Route path="conversation/:id" element={<Pages.ConversationPage />} />
+            </Route>
+          </Routes>
+        </ConversationsProvider>
+      </ThemeProvider>
+    </AuthProvider>
   );
 }
