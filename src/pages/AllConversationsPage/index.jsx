bf3218754f6041892e67719642ab4a8db4fab829
Rahma Ali <rahma.ma078@gmail.com>
2024-03-25 11:23:00 +0000

    updating 'all conversations' page

--- src/pages/AllConversationsPage/index.jsx
+++ src/pages/AllConversationsPage/index.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import AllConversationsGrid from "../../components/Grid";
+import { Link } from 'react-router-dom';
+import AllConversationsGrid from "../../components/AllConversations";
 import NewChatBtn from '../../components/NewChatBtn';
 import "./allconversations.css"
 
 export default function AllConversationsPage() {
 
   return (
-    <>
+    <div id="convo">
+    <Link></Link>
     <NewChatBtn />
     <AllConversationsGrid />
-  </>
+    </div>
   );
 }
