9f7192da5fc605f256a06507fc376afda80c8409
malak <malookaabdalla@gmail.com>
2024-03-25 13:24:59 +0000

    color fix

--- src/layouts/LanguageLayout/index.jsx
+++ src/layouts/LanguageLayout/index.jsx
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
 import LanguageHeading from "../../components/LanguageHeading";
 import LanguageSelector from "../../components/Languages";
 
 function LanguageLayout() {
+  useEffect(() => {
+    // Add class to body element when component mounts
+    document.body.classList.add("page-loaded");
+
+    // Cleanup function to remove the class when component unmounts
+    return () => {
+      document.body.classList.remove("page-loaded");
+    };
+  }, []);
   return (
     <>
       <LanguageHeading />
