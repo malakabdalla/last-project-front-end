3275e31d63c576b668dee2ae0476aab6fc8575d0
hortfrancis <ajhf@hotmail.co.uk>
2024-03-26 10:56:52 +0000

    Fix centering of log in card

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -2,9 +2,9 @@ import LoginCard from "../../components/logincard";
 
 function LoginPage() {
   return (
-    <>
+    <div className="min-h-screen flex justify-center items-center  bg-gradient-to-br from-pink-100 to-pink-300">
       <LoginCard />
-    </>
+    </div>
   );
 }
 
